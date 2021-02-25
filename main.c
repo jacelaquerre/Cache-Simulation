569d70100d6fa93cf10cbe5989b40bb41f01f171
Jace Laquerre <jace.laquerre@uvm.edu>
2021-02-25 15:30:28 -0500

    started making cache array

--- main.c
+++ main.c
@@ -2,3 +2,30 @@
 // Created by jacel on 2/18/2021.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char memory[1024];
+void initialize();
+
+int main() {
+    initialize();
+    return 0;
+}
+
+void initialize() {
+    int i;
+    int ct = 49;
+    for (i = 0; i < sizeof(memory); ++i) {
+        if (!(i & 3) || i == 0) {
+            //++ct;
+            memory[i] = ct;
+        } else {
+            memory[i] = '0';
+        }
+    }
+
+    printf(memory);
+}
