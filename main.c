429e2dec5018143628c1604406a830c16d42e981
Jace Laquerre <jace.laquerre@uvm.edu>
2021-02-25 20:49:45 -0500

    making cache array

--- main.c
+++ main.c
@@ -7,8 +7,15 @@
 #include <string.h>
 #include <math.h>
 
-char memory[1024];
+int memory[1024];
 void initialize();
+
+typedef struct Word {
+    int word;
+} Word;
+
+Word readWord(unsigned int address);
+void writeWord(unsigned int address, Word word);
 
 int main() {
     initialize();
@@ -16,16 +23,30 @@ int main() {
 }
 
 void initialize() {
+    int size = sizeof memory / sizeof memory[0];
     int i;
-    int ct = 49;
-    for (i = 0; i < sizeof(memory); ++i) {
+    int ct = 0;
+    for (i = 0; i < size; ++i) {
         if (!(i & 3) || i == 0) {
-            //++ct;
+            ++ct;
             memory[i] = ct;
         } else {
-            memory[i] = '0';
+            memory[i] = 0;
         }
     }
+    // Print Array
+    for(i = 0; i < size; ++i) {
+        if (!(i & 3) && i != 0) {
+            printf(" ");
+        }
+        printf("%d", memory[i]);
+    }
+}
 
-    printf(memory);
+Word readWord(unsigned int address) {
+
+}
+
+void writeWord(unsigned int address, Word word) {
+
 }
