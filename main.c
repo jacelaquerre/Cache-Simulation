9db9096fd9e54d0cc9cf8f5b7eb172c896590809
Jace Laquerre <jace.laquerre@uvm.edu>
2021-02-28 21:52:01 -0500

    more work

--- main.c
+++ main.c
@@ -8,32 +8,53 @@
 #include <math.h>
 
 // the size of a memory address, in bits
-const int MEMORY_SIZE = 1024;
+#define MEMORY_SIZE 65536
 // the size of the cache, in bytes
-const int CACHE_SIZE = 1000;
+#define CACHE_SIZE  1024
 // this size of a cache block, in bytes
-const int BLOCK_SIZE = 64;
+#define BLOCK_SIZE 64
 // the associativity of the cache
-const int ASSOCIATIVITY = 1;
+#define ASSOCIATIVITY 1
 // Write-Back = 0
 // Write-Through = 1
-const int WRITE_BACK = 0;
+#define WRITE_BACK 0
 
-const int TAG_LENGTH = 8;
+#define TAG_LENGTH 8
 
 int memory[MEMORY_SIZE];
-int cache[CACHE_SIZE];
-void initialize();
 
 typedef struct Word {
     int word;
 } Word;
 
+typedef struct CacheBlock {
+    int cacheBlock[BLOCK_SIZE];
+    int tag;
+    // 0 - not valid, 1 = valid
+    int valid;
+    // 0 - not dirty, 1 = dirty
+    int dirty;
+} CacheBlock;
+
+typedef struct CacheSet {
+    CacheBlock cacheBlock[CACHE_SIZE/BLOCK_SIZE];  // # of blocks
+    int tagQueue[(CACHE_SIZE/BLOCK_SIZE) * ASSOCIATIVITY];  // # of sets = # blocks * associativity
+} CacheSet;
+
+typedef struct Cache {
+    CacheSet cache[CACHE_SIZE];
+    // Write-Back = 0, Write-Through = 1
+    int write;
+} Cache;
+
 Word readWord(unsigned int address);
 void writeWord(unsigned int address, Word word);
+void initialize();
+void output();
 
 int main() {
     initialize();
+    output();
     return 0;
 }
 
@@ -50,12 +71,27 @@ void initialize() {
         }
     }
     // Print Array
-    for (i = 0; i < size; ++i) {
-        if (!(i & 3) && i != 0) {
-            printf(" ");
-        }
-        printf("%d", memory[i]);
+//    for (i = 0; i < size; ++i) {
+//        if (!(i & 3) && i != 0) {
+//            printf(" ");
+//        }
+//        printf("%d", memory[i]);
+//    }
+}
+
+void output() {
+    printf("-----------------------------------\n");
+    printf("Cache Size = %d\n", CACHE_SIZE);
+    printf("Block Size = %d\n", BLOCK_SIZE);
+    printf("#Blocks = %d\n", CACHE_SIZE / BLOCK_SIZE);
+    printf("#Sets = %d\n", 0);
+    printf("Associativity = %d\n", ASSOCIATIVITY);
+    if (WRITE_BACK == 0) {
+        printf("Write Back\n");
+    } else {
+        printf("Write Through\n");
     }
+    printf("-----------------------------------");
 }
 
 Word readWord(unsigned int address) {
@@ -63,7 +99,10 @@ Word readWord(unsigned int address) {
     int byte_range = memory_block * BLOCK_SIZE;
     int slot = (byte_range / BLOCK_SIZE) % TAG_LENGTH;
     int tag = (byte_range / BLOCK_SIZE) / TAG_LENGTH;
-
+    int i;
+    for (i = 0; i < (byte_range + (BLOCK_SIZE - 1)); ++i) {
+        //memory[i] = cache[slot];
+    }
 }
 
 void writeWord(unsigned int address, Word word) {
