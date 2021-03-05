b3e17075b959d38f60c44db9da8c758e71b7f52e
Jace Laquerre <jace.laquerre@uvm.edu>
2021-03-04 21:46:49 -0500

    more work

--- cache.c
+++ cache.c
@@ -9,15 +9,40 @@
 #include <ctype.h>
 #include "cache.h"
 
+// Initialize Structs
+CacheBlock cacheBlock = { .valid = 0, .dirty = 0, .tag = 0, .cacheBlock = {} };
+CacheSet cacheSet = { .tagQueue = {}, .cacheBlocks = {} };
+Cache cache = { .cacheSets = {}, .write = WRITE_BACK };
+
 int main() {
-//    CacheBlock cacheBlock = { .write = WRITE_BACK, .cache = 123 };
-//    CacheSet cacheSet = { .tagQueue = [(CACHE_SIZE/BLOCK_SIZE) * ASSOCIATIVITY], .cache = 123 };
-//    Cache cache = { .write = WRITE_BACK, .cache = cacheSet };
     initialize();
     output();
-    readWord(46916);
-    displayBinary(46916);
-    //printf("%s", binaryConverter(2460));
+
+    printf("Several addresses that map to the same set:");
+    readWord(1152);
+    readWord(2176);
+    readWord(3200);
+    readWord(4224);
+    readWord(5248);
+    readWord(7296);
+    readWord(4224);
+    readWord(3200);
+
+    Word word = { .word = 17};
+    writeWord(7312, word);
+
+    readWord(7320);
+    readWord(4228);
+    readWord(3212);
+
+    word.word = 5;
+    writeWord(5248, word);
+
+    readWord(5248);
+
+    word.word = 7;
+    writeWord(8320, word);
+
     return 0;
 }
 
@@ -44,6 +69,19 @@ void initialize() {
 //        }
 //        printf("%d", memory[i]);
 //    }
+    // Initialize Structs
+    // # of blocks
+    for (i = 0; i < (CACHE_SIZE / BLOCK_SIZE); ++i) {
+        cacheSet.cacheBlocks[i] = cacheBlock;
+    }
+    for (i = 0; i < (CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY; ++i) {
+        cacheSet.tagQueue[i] = -1;
+    }
+    // # of sets
+    for (i = 0; i < ((CACHE_SIZE / BLOCK_SIZE) / ((CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY)); ++i) {
+        cache.cacheSets[i] = cacheSet;
+    }
+
 }
 
 void output() {
@@ -51,9 +89,9 @@ void output() {
     printf("Cache Size = %d\n", CACHE_SIZE);
     printf("Block Size = %d\n", BLOCK_SIZE);
     printf("#Blocks = %d\n", CACHE_SIZE / BLOCK_SIZE);
-    printf("#Sets = %d\n", (CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY);
+    printf("#Sets = %d\n", (CACHE_SIZE / BLOCK_SIZE) / ASSOCIATIVITY);
     printf("Associativity = %d\n", ASSOCIATIVITY);
-    printf("Tag Length = %.0f\n", (CACHE_SIZE/BLOCK_SIZE) - (log2((CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY)) - (log2(BLOCK_SIZE)));
+    printf("Tag Length = %.0f\n", (CACHE_SIZE / BLOCK_SIZE) - (log2((CACHE_SIZE / BLOCK_SIZE) / ASSOCIATIVITY)) - (log2(BLOCK_SIZE)));
     if (WRITE_BACK == 0) {
         printf("Write Back\n");
     } else {
@@ -87,33 +125,101 @@ Word readWord(unsigned int address) {
         Word word = { .word = -1};
         return word;
     }
+    //    CacheBlock cacheBlock = { .valid = 1, .dirty = 0, .tag = 0, .cacheBlock = {} };
+    //    CacheSet cacheSet = { .tagQueue = {}, .cacheBlocks = {} };
+    //    Cache cache = { .cacheSets = {}, .write = WRITE_BACK };
     int memory_block = (int)address / BLOCK_SIZE;
-    printf("memory_block %d\n", memory_block);
     int byte_range = memory_block * BLOCK_SIZE;
-    printf("byte_range %d\n", byte_range);
-    float tagLength = (CACHE_SIZE/BLOCK_SIZE) - (log2((CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY)) - (log2(BLOCK_SIZE));
-    printf("tegLength %d\n", (int) tagLength);
-    int slot = ((int)address / BLOCK_SIZE) % (int) tagLength;
-    printf("slot %d\n", slot);
-    int tag = (((int)address / BLOCK_SIZE) / (int) tagLength);
-    printf("tag %d\n", tag);
-    int blockIndex = 0;
+    float tagLength = (CACHE_SIZE / BLOCK_SIZE) - (log2((CACHE_SIZE / BLOCK_SIZE) / ASSOCIATIVITY)) - (log2(BLOCK_SIZE));
+    int slot = ((int)address / BLOCK_SIZE) % ((CACHE_SIZE / BLOCK_SIZE) / ASSOCIATIVITY);
+    int tag = ((int)address / BLOCK_SIZE) / ((CACHE_SIZE / BLOCK_SIZE) / ASSOCIATIVITY);
+    int blockIndex = -1;
+    int evictedTag = -1;
+    int i;
+
+    // Update Tag Queue
+    int tagAdded = 0;
+    int readHit = 0;
+    for (i = 0; i < (CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY; ++i) {
+        // Check for read hit
+        if(cache.cacheSets[slot].tagQueue[i] == tag) {
+            readHit = 1;
+            break;
+        }
+        if (cache.cacheSets[slot].tagQueue[i] == -1) {
+            cache.cacheSets[slot].tagQueue[i] = tag;
+            tagAdded = 1;
+            blockIndex = i;
+            break;
+        }
+    }
+    // LRU
+    if (tagAdded == 0 && readHit == 0) {
+        if (lastReplacedIndex == -1) {
+            lastReplacedIndex = 0;
+        } else {
+            ++lastReplacedIndex;
+        }
+        evictedTag = cache.cacheSets[slot].tagQueue[lastReplacedIndex];
+        blockIndex = lastReplacedIndex;
+        cache.cacheSets[slot].tagQueue[lastReplacedIndex] = tag;
+    }
+
+    if (readHit == 0) {
+        // Move 64 byte from memory to cache block
+        for (i = byte_range; i < (byte_range + (BLOCK_SIZE - 1)); ++i) {
+            cache.cacheSets[slot].cacheBlocks->cacheBlock[i - byte_range] = memory[i];
+        }
+        // Update tag in block
+        cache.cacheSets[slot].cacheBlocks->tag = tag;
+        // Update valid block
+        cache.cacheSets[slot].cacheBlocks->valid = 1;
+    }
+    Word word = { .word = 0};
+    // Get word
+    if (cache.cacheSets[slot].cacheBlocks->dirty == 1) {
+        word.word = cache.cacheSets[slot].cacheBlocks->cacheBlock[(int) address + 1];
+    } else {
+        word.word = (int) address;
+    }
     // OUTPUT
-    printf("[addr=%d", (int)address);
+//    printf("memory_block %d\n", memory_block);
+//    printf("byte_range %d\n", byte_range);
+//    printf("tegLength %d\n", (int) tagLength);
+//    printf("slot %d\n", slot);
+//    printf("tag %d\n", tag);
+    printf("\n(R) address is: %d ", (int)address);
+    displayBinary((int)address);
+    printf("\n[addr=%d", (int)address);
     printf(" index=%d", slot);
     printf(" blockIndex=%d", blockIndex);
     printf(" tag=%d", tag);
+    if (tagAdded == 1) {
+        printf(" read miss;");
+    } else if (readHit == 1) {
+        printf(" read hit;");
+    } else {
+        printf(" read miss + replace;");
+    }
     // read miss etc
-    printf(" word=%d", (int)address);
+    printf(" word=%d", word.word);
     printf(" (%d", byte_range);
-    printf(" - %d)]\n", (byte_range + (BLOCK_SIZE - 1)));
-    int i;
-//    for (i = 0; i < (byte_range + (BLOCK_SIZE - 1)); ++i) {
-//        //memory[i] = cache[slot];
-//    }
-    //int wordArray[4] =
-    //for (i = 0; i < (byte_range + (BLOCK_SIZE - 1)); ++i) {
-    Word word = { .word = (int)address};
+    printf(" - %d)]", (byte_range + (BLOCK_SIZE - 1)));
+
+    // Print tag evict message
+    if (tagAdded == 0 && readHit == 0) {
+        printf("\nEvict tag %d", evictedTag);
+        printf(", in blackIndex %d", blockIndex);
+    }
+
+    // Print tagQueue
+    printf("\n[");
+    for (i = 0; i < ((CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY); ++i) {
+        printf(" %d", cache.cacheSets[slot].tagQueue[i]);
+    }
+    printf(" ]");
+    printf("\n");
+
     return word;
 }
 
@@ -121,4 +227,97 @@ void writeWord(unsigned int address, Word word) {
     if (validation((int)address) == 0) {
         printf("Address not 4 byte aligned or in valid memory range!");
     }
+    //    CacheBlock cacheBlock = { .valid = 1, .dirty = 0, .tag = 0, .cacheBlock = {} };
+    //    CacheSet cacheSet = { .tagQueue = {}, .cacheBlocks = {} };
+    //    Cache cache = { .cacheSets = {}, .write = WRITE_BACK };
+    int memory_block = (int)address / BLOCK_SIZE;
+    int byte_range = memory_block * BLOCK_SIZE;
+    float tagLength = (CACHE_SIZE / BLOCK_SIZE) - (log2((CACHE_SIZE / BLOCK_SIZE) / ASSOCIATIVITY)) - (log2(BLOCK_SIZE));
+    int slot = ((int)address / BLOCK_SIZE) % ((CACHE_SIZE / BLOCK_SIZE) / ASSOCIATIVITY);
+    int tag = ((int)address / BLOCK_SIZE) / ((CACHE_SIZE / BLOCK_SIZE) / ASSOCIATIVITY);
+    int blockIndex = -1;
+    int evictedTag = -1;
+    int i;
+
+    // Update Tag Queue
+    int tagAdded = 0;
+    int writeHit = 0;
+    for (i = 0; i < (CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY; ++i) {
+        // Check for read hit
+        if(cache.cacheSets[slot].tagQueue[i] == tag) {
+            writeHit = 1;
+            break;
+        }
+        if (cache.cacheSets[slot].tagQueue[i] == -1) {
+            cache.cacheSets[slot].tagQueue[i] = tag;
+            tagAdded = 1;
+            blockIndex = i;
+            break;
+        }
+    }
+    // LRU
+    if (tagAdded == 0 && writeHit == 0) {
+        if (lastReplacedIndex == -1) {
+            lastReplacedIndex = 0;
+        } else {
+            ++lastReplacedIndex;
+        }
+        evictedTag = cache.cacheSets[slot].tagQueue[lastReplacedIndex];
+        blockIndex = lastReplacedIndex;
+        cache.cacheSets[slot].tagQueue[lastReplacedIndex] = tag;
+    }
+
+    // Move from memory on write miss
+    if (writeHit == 0) {
+        // Move 64 byte from memory to cache block
+        for (i = byte_range; i < (byte_range + (BLOCK_SIZE - 1)); ++i) {
+            cache.cacheSets[slot].cacheBlocks->cacheBlock[i - byte_range] = memory[i];
+        }
+        // Update tag in block
+        cache.cacheSets[slot].cacheBlocks->tag = tag;
+        // Update valid block
+        cache.cacheSets[slot].cacheBlocks->valid = 1;
+    }
+    // Write Word
+    cache.cacheSets[slot].cacheBlocks->cacheBlock[address + 1] = word.word;
+    // Make it dirty
+    cache.cacheSets[slot].cacheBlocks->dirty = 1;
+
+    // OUTPUT
+//    printf("memory_block %d\n", memory_block);
+//    printf("byte_range %d\n", byte_range);
+//    printf("tegLength %d\n", (int) tagLength);
+//    printf("slot %d\n", slot);
+//    printf("tag %d\n", tag);
+    printf("\n(W) address is: %d ", (int)address);
+    displayBinary((int)address);
+    printf("\n[addr=%d", (int)address);
+    printf(" index=%d", slot);
+    printf(" blockIndex=%d", blockIndex);
+    printf(" tag=%d", tag);
+    if (tagAdded == 1) {
+        printf(" write miss;");
+    } else if (writeHit == 1) {
+        printf(" write hit;");
+    } else {
+        printf(" write miss + replace;");
+    }
+    // read miss etc
+    printf(" word=%d", word.word);
+    printf(" (%d", byte_range);
+    printf(" - %d)]", (byte_range + (BLOCK_SIZE - 1)));
+
+    // Print tag evict message
+    if (tagAdded == 0 && writeHit == 0) {
+        printf("\nEvict tag %d", evictedTag);
+        printf(", in blackIndex %d", blockIndex);
+    }
+
+    // Print tagQueue
+    printf("\n[");
+    for (i = 0; i < ((CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY); ++i) {
+        printf(" %d", cache.cacheSets[slot].tagQueue[i]);
+    }
+    printf(" ]");
+    printf("\n");
 }
--- cache.h
+++ cache.h
@@ -7,12 +7,12 @@
 
 // the size of a memory address, in bits
 #define MEMORY_SIZE  65536
-// the size of the cache, in bytes
+// the size of the cacheSets, in bytes
 #define CACHE_SIZE  1024
-// this size of a cache block, in bytes
+// this size of a cacheSets block, in bytes
 #define BLOCK_SIZE 64
-// the associativity of the cache
-#define ASSOCIATIVITY 1
+// the associativity of the cacheSets
+#define ASSOCIATIVITY 4
 // Write-Back = 0
 // Write-Through = 1
 #define WRITE_BACK 0
@@ -26,19 +26,19 @@ typedef struct Word {
 typedef struct CacheBlock {
     int cacheBlock[BLOCK_SIZE];
     int tag;
-    // 0 - not valid, 1 = valid
+    // 0 - valid, 1 = not valid
     int valid;
     // 0 - not dirty, 1 = dirty
     int dirty;
 } CacheBlock;
 
 typedef struct CacheSet {
-    CacheBlock cacheBlock[CACHE_SIZE/BLOCK_SIZE];  // # of blocks
+    CacheBlock cacheBlocks[((CACHE_SIZE / BLOCK_SIZE) / ((CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY))];  // # of blocks divided by sets
     int tagQueue[(CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY]; // # of sets
 } CacheSet;
 
 typedef struct Cache {
-    CacheSet cache[CACHE_SIZE];
+    CacheSet cacheSets[(CACHE_SIZE/BLOCK_SIZE) / ASSOCIATIVITY];
     // Write-Back = 0, Write-Through = 1
     int write;
 } Cache;
@@ -49,6 +49,9 @@ void initialize();
 void output();
 int validation(int address);
 void displayBinary(int n);
+
+// For Evicting
+int lastReplacedIndex = -1;
 
 #endif //CAHCE_SIMULATION_CACHE_H
 
